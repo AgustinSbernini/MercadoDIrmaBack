d2aca3998938884967d6e7cb8e3edeeec5c79e55
Agustin Sbernini <fliasbernini@hotmail.com>
2023-08-17 22:56:28 +0000

    ABM-Producto

--- mercado-dirma-backend/Queries/DaoBase.cs
+++ mercado-dirma-backend/Queries/DaoBase.cs
@@ -5,7 +5,7 @@ namespace mercado_dirma_backend.Queries
     public class DaoBase
     {
         private static string stringConeccion = @"Server=tcp:lucasanche-server.database.windows.net,1433;Initial Catalog=Mercado_DIrma;Persist Security Info=False;User ID=ls-admin;Password=Grupo13prog;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        public static SqlConnection Coneccion()
+        public static SqlConnection GetConection()
         {
             var db = new SqlConnection(stringConeccion);
             db.Open();
---
+++ mercado-dirma-backend/Queries/ProductBrandQuery.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using mercado_dirma_backend.Models;
+using System.Text;
+
+namespace mercado_dirma_backend.Queries
+{
+    public class ProductBrandQuery : DaoBase
+    {
+        public static async Task<IEnumerable<ProductBrand>> GetAll(bool isActive)
+        {
+            var sql = new StringBuilder(@"
+                SELECT IdProductBrand
+                    , Name
+                    , Active
+                FROM ProductBrand ");
+
+            if(isActive)
+            {
+                sql.Append(" WHERE Active = 1 ");
+            }
+
+            using(var db = GetConection())
+            {
+                return await db.QueryAsync<ProductBrand>(sql.ToString());
+            }
+        }
+
+        public static async Task<ProductBrand> GetById(int idProductBrand)
+        {
+            var sql = @"
+                SELECT IdProductBrand
+                    , Name
+                    , Active
+                FROM ProductBrand
+                WHERE IdProductBrand = @IdProductBrand
+                    AND Active = 1                   
+            ";
+
+            using (var db = GetConection())
+            {
+                return await db.QueryFirstOrDefaultAsync<ProductBrand>(sql, new { IdProductBrand = idProductBrand });
+            }
+        }
+    }
+}
