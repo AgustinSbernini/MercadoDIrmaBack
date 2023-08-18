46b9113ba6a4fa50c8aa5cc550457b975fc3531e
Julian <87672293+Casablancas01@users.noreply.github.com>
2023-08-17 23:47:36 -0300

    ABM-ProductType

---
+++ mercado-dirma-backend/Queries/ProductTypeQuery.cs
@@ -0,0 +1,45 @@
+﻿using mercado_dirma_backend.Models;
+using System.Text;
+using Dapper;
+
+namespace mercado_dirma_backend.Queries
+{
+    public class ProductTypeQuery : DaoBase
+    {
+        public static async Task<IEnumerable<ProductType>> GetAll(bool isActive)
+        {
+            var sql = new StringBuilder(@"
+                SELECT IdProductType
+                    , Name
+                    , Active
+                FROM ProductType ");
+
+            if (isActive)
+            {
+                sql.Append(" WHERE Active = 1 ");
+            }
+
+            using (var db = GetConection())
+            {
+                return await db.QueryAsync<ProductType>(sql.ToString());
+            }
+        }
+
+        public static async Task<ProductType> GetById(int idProductType)
+        {
+            var sql = @"
+                SELECT IdProductType
+                    , Name
+                    , Active
+                FROM ProductType
+                WHERE IdProductType = @IdProductType
+                    AND Active = 1                   
+            ";
+
+            using (var db = GetConection())
+            {
+                return await db.QueryFirstOrDefaultAsync<ProductType>(sql, new { IdProductType = idProductType });
+            }
+        }
+    }
+}
