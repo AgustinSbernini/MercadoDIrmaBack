375aa3d1e5142b6116cd3778891528061148c732
Julian Casablancas <julian_asurmendi@hotmail.com>
2023-08-23 20:36:05 +0000

    ABM product status

---
+++ mercado-dirma-backend/Commands/ProductStatusCommand.cs
@@ -0,0 +1,49 @@
+﻿using Dapper;
+using mercado_dirma_backend.Models;
+using mercado_dirma_backend.Queries;
+
+namespace mercado_dirma_backend.Commands
+{
+    public class ProductStatusCommand: DaoBase
+    {
+        public static async Task<bool> Insert(ProductStatus productStatus)
+        {
+            var sql = @"
+                INSERT INTO ProductStatus
+                    (Name, Active)
+                VALUES (@Name, 1)";
+
+            using (var db = GetConection())
+            {
+                return await db.ExecuteAsync(sql, new { productStatus.Name }) == 1;
+            }
+        }
+
+        public static async Task<bool> Delete(int idProductStatus)
+        {
+            var sql = @"
+                UPDATE ProductStatus
+                SET Active = 0 
+                WHERE IdProductStatus = @IdProductStatus";
+
+            using (var db = GetConection())
+            {
+                return await db.ExecuteAsync(sql, new { IdProductStatus = idProductStatus }) == 1;
+            }
+        }
+
+        public static async Task<bool> Update(ProductStatus productStatus)
+        {
+            var sql = @"
+                UPDATE ProductStatus 
+                SET Name = @Name
+                    , Active = @Active
+                WHERE IdProductStatus = @IdProductStatus";
+
+            using (var db = GetConection())
+            {
+                return await db.ExecuteAsync(sql, new { productStatus.IdProductStatus, productStatus.Name, productStatus.Active }) == 1;
+            }
+        }
+    }
+}
--- mercado-dirma-backend/Queries/DaoBase.cs
+++ mercado-dirma-backend/Queries/DaoBase.cs
@@ -4,7 +4,7 @@ namespace mercado_dirma_backend.Queries
 {
     public class DaoBase
     {
-        private static string stringConeccion = @"Server=tcp:asbernini-db.database.windows.net,1433;Initial Catalog=Mercado-DIrma;Persist Security Info=False;User ID=sa-sysadmin;Password=C0ntr4L0c4!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=60;";
+        private static string stringConeccion = @"Server=tcp:asbernini-db.database.windows.net,1433;Initial Catalog=Mercado_DIrma;Persist Security Info=False;User ID=sa-sysadmin;Password=C0ntr4L0c4!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=60;";
 
         public static SqlConnection GetConection()
         {
