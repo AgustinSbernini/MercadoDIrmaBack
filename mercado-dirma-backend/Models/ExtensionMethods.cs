0fd2aadff30e50e76eb306ee7d95a3bb4dddc0e0
Agustin Sbernini <fliasbernini@hotmail.com>
2023-08-24 00:12:20 +0000

    ABM-Availability

---
+++ mercado-dirma-backend/Models/ExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mercado_dirma_backend.Models
+{
+    public static class ExtensionMethods
+    {
+        public static string GetName(this Day value)
+        {
+            var fieldInfo = value.GetType().GetField(value.ToString());
+
+            var displayAttribute = fieldInfo?.GetCustomAttributes(
+                typeof(DisplayAttribute), false
+            ) as DisplayAttribute[];
+
+            if (displayAttribute == null || displayAttribute.Length == 0)
+            {
+                return value.ToString(); // Si no se encuentra el atributo, devolver el nombre del enum
+            }
+
+            return displayAttribute[0].GetName();
+        }
+    }
+}
