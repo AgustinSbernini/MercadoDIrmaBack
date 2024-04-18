ab135aa5ed6243d4e19cf9bade06983fc0c4686d
Agustin Sbernini <98596043+AgustinSbernini@users.noreply.github.com>
2024-04-18 20:31:19 -0300

    Update DaoBase.cs
    Segunda prueba de base de datos

--- mercado-dirma-backend/DataAccess/DaoBase.cs
+++ mercado-dirma-backend/DataAccess/DaoBase.cs
@@ -4,7 +4,7 @@ namespace mercado_dirma_backend.DataAccess
 {
     public class DaoBase
     {
-        private static string stringConeccion = @"workstation id=mercadodirma.mssql.somee.com;packet size=4096;user id=agustinsbernini_SQLLogin_1;pwd=449o4huunn;data source=mercadodirma.mssql.somee.com;persist security info=False;initial catalog=mercadodirma;TrustServerCertificate=True";
+        private static string stringConeccion = @"Server=tcp:mercadodirma.mssql.somee.com,1433;Initial Catalog=mercadodirma;Persist Security Info=False;User ID=agustinsbernini_SQLLogin_1;Password=449o4huunn;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=60;";
 
         public static SqlConnection GetConection()
         {
